dc44800980125187b23161914aaed9807750351e
y0ast <joost@joo.st>
2014-03-04 19:37:18 +0100

    remove double header

--- eonn/eonn.py
+++ eonn/eonn.py
@@ -9,41 +9,6 @@
 #
 # Eonn is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this library.	If not, see <http://www.gnu.org/licenses/>.
-
-"""
-Main module, implementing a complete generational evolutionary algorithm.
-"""
-
-import random
-from organism import *
-import numpy as np
-from sklearn.gaussian_process import *
-import random
-from pprint import *
-
-import matplotlib.pyplot as plt
-try:
-	from mpltools import style
-	style.use('ggplot')
-except:
-	pass
-
-# Copyright (C) 2009 Rogier Koppejan <rogier.koppejan@gmail.com>
-#
-# This file is part of eonn.
-#
-# Eonn is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Eonn is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
